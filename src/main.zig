e03a26610015bb0ff861cb5d831fb04f439280e7
Ben Grant <benjamin.j.grant@gmail.com>
2022-03-24 15:18:37 -0700

    Initial image output

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,25 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
+    const imageWidth = 256;
+    const imageHeight = 256;
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+    const writer = std.io.getStdOut().writer();
+    try writer.print("P3\n{} {}\n255\n", .{ imageWidth, imageHeight });
+
+    var j: i32 = imageHeight - 1;
+    while (j >= 0) : (j -= 1) {
+        var i: u32 = 0;
+        while (i < imageWidth) : (i += 1) {
+            const r = @intToFloat(f64, i) / (imageWidth - 1);
+            const g = @intToFloat(f64, j) / (imageHeight - 1);
+            const b = 0.25;
+
+            const ir = @floatToInt(u32, 255.999 * r);
+            const ig = @floatToInt(u32, 255.999 * g);
+            const ib = @floatToInt(u32, 255.999 * b);
+
+            try writer.print("{} {} {}\n", .{ ir, ig, ib });
+        }
+    }
 }

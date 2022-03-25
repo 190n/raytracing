f29754fced733582d841b23521aee0479cc0cc23
Ben Grant <benjamin.j.grant@gmail.com>
2022-03-24 18:04:54 -0700

    Blue-white gradient with rays

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,34 @@ const std = @import("std");
 
 const Vec3 = @import("./Vec3.zig");
 const Color = Vec3.Color;
+const Point3 = Vec3.Point3;
 const writeColor = @import("./color.zig").writeColor;
+const Ray = @import("./Ray.zig");
+
+fn rayColor(r: *const Ray) Color {
+    const unit_direction = r.dir.unitVector();
+    const t = 0.5 * (unit_direction.y() + 1.0);
+    return Color.create(1.0, 1.0, 1.0).mulScalar(1.0 - t).add(&Color.create(0.5, 0.7, 1.0).mulScalar(t));
+}
 
 pub fn main() anyerror!void {
-    const image_width = 256;
-    const image_height = 256;
+    // image
+    const aspect_ratio = 16.0 / 9.0;
+    const image_width = 400;
+    const image_height = @floatToInt(comptime_int, image_width / aspect_ratio);
+
+    // camera
+    const viewport_height = 2.0;
+    const viewport_width = aspect_ratio * viewport_height;
+    const focal_length = 1.0;
+
+    const origin = Point3.zero();
+    const horizontal = Vec3.create(viewport_width, 0.0, 0.0);
+    const vertical = Vec3.create(0.0, viewport_height, 0.0);
+    const lower_left_corner = origin
+        .sub(&horizontal.divScalar(2.0))
+        .sub(&vertical.divScalar(2.0))
+        .sub(&Vec3.create(0.0, 0.0, focal_length));
 
     var buf = std.io.bufferedWriter(std.io.getStdOut().writer());
     const writer = buf.writer();
@@ -20,11 +43,13 @@ pub fn main() anyerror!void {
 
         var i: u32 = 0;
         while (i < image_width) : (i += 1) {
-            const pixel_color = Color.create(
-                @intToFloat(f64, i) / (image_width - 1),
-                @intToFloat(f64, j) / (image_height - 1),
-                0.25,
-            );
+            const u = @intToFloat(f64, i) / (image_width - 1);
+            const v = @intToFloat(f64, j) / (image_height - 1);
+            const r = Ray.create(&origin, &lower_left_corner
+                .add(&horizontal.mulScalar(u))
+                .add(&vertical.mulScalar(v))
+                .sub(&origin));
+            const pixel_color = rayColor(&r);
             try writeColor(writer, &pixel_color);
         }
     }
